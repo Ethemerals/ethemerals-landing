f149c983c5273bf919d4c9ac730ed420d419e8fe
tyrone <binktogether@gmail.com>
2021-05-13 01:14:20 -0700

    added h-full div

--- components/sections/Land.js
+++ components/sections/Land.js
@@ -2,9 +2,11 @@ const Land = () => {
 	return (
 		<section className="bg-land bg-cover bg-center text-center h-screen text-gray-300 relative">
 			<a id="land">
-				<div className="absolute m-0 top-2/4 w-full">
-					<h1 className="text-5xl md:text-7xl lg:text-9xl">Land and Item sales</h1>
-					<p className="text-xl md:text-2xl lg:text-4xl pt-2">coming soon!</p>
+				<div className="h-full">
+					<div className="absolute m-0 top-2/4 w-full">
+						<h1 className="text-5xl md:text-7xl lg:text-9xl">Land and Item sales</h1>
+						<p className="text-xl md:text-2xl lg:text-4xl pt-2">coming soon!</p>
+					</div>
 				</div>
 			</a>
 		</section>
