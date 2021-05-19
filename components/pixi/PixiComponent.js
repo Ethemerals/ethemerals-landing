bb60e43320c68453eed279cc2dec426b56092f3f
tyrone <binktogether@gmail.com>
2021-05-19 14:07:05 -0700

    fixed sizes

--- components/pixi/PixiComponent.js
+++ components/pixi/PixiComponent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as PIXI from 'pixi.js';
+
 // import PixiApp from './PixiApp';
 // import { Application } from '@pixi/app';
 // import { AppLoaderPlugin } from '@pixi/loaders';
@@ -10,49 +11,20 @@ const app = new PIXI.Application({
 	autoResize: true,
 	backgroundColor: 0x2998c2,
 	antialias: true,
-	transparent: false,
-	resolution: devicePixelRatio,
+	backgroundAlpha: true,
+	resolution: 1,
 });
 
 function resize(width, scale) {
-	app.renderer.resize(width, width);
 	app.stage.scale.set(scale);
-}
-
-function run() {
-	console.log('hello');
-	console.log('loaded running');
-
-	try {
-		app.renderer.plugins.interaction.autoPreventDefault = false;
-		app.renderer.view.style.touchAction = 'auto';
-		let sprite = new PIXI.Sprite(app.loader.resources.char1.texture);
-
-		sprite.anchor.set(0.5);
-
-		// Move the sprite to the center of the screen
-		sprite.x = app.renderer.width / 2;
-		sprite.y = app.renderer.height / 2;
-
-		app.stage.addChild(sprite);
-
-		// Listen for animate update
-		app.ticker.add(function (delta) {
-			// Rotate mr rabbit clockwise
-			sprite.rotation += 0.01 * delta;
-		});
-	} catch (error) {
-		console.log(error);
-	}
-
-	console.log('loaded end');
+	app.renderer.resize(width, width);
 }
 
 const PixiComponent = () => {
 	const gameCanvas = useRef(null);
 
 	const maxWidth = 800;
-	const [windowWidth, setWindowWidth] = useState(maxWidth);
+	const [windowWidth, setWindowWidth] = useState(800);
 	const [stageWidth, setStageWidth] = useState(windowWidth);
 	const [scaleWidth, setScaleWidth] = useState(1);
 
@@ -82,15 +54,43 @@ const PixiComponent = () => {
 	}, [stageWidth]);
 
 	useEffect(() => {
-		resize(stageWidth, scaleWidth);
-	});
-
-	useEffect(() => {
 		if (gameCanvas.current.childNodes.length < 1) {
 			gameCanvas.current.appendChild(app.view);
+			resize(stageWidth, scaleWidth);
 			app.loader.add('char1', './pixi/char1.png').load(run);
 		}
-	}, []);
+
+		resize(stageWidth, scaleWidth);
+	}, [scaleWidth]);
+
+	function run() {
+		console.log('hello');
+		console.log('loaded running');
+
+		try {
+			app.renderer.plugins.interaction.autoPreventDefault = false;
+			app.renderer.view.style.touchAction = 'auto';
+			let sprite = new PIXI.Sprite(app.loader.resources.char1.texture);
+
+			sprite.anchor.set(0.5);
+
+			// Move the sprite to the center of the screen
+			sprite.x = stageWidth / 2;
+			sprite.y = stageWidth / 2;
+
+			app.stage.addChild(sprite);
+
+			// Listen for animate update
+			app.ticker.add(function (delta) {
+				// Rotate mr rabbit clockwise
+				sprite.rotation += 0.01 * delta;
+			});
+		} catch (error) {
+			console.log(error);
+		}
+
+		console.log('loaded end');
+	}
 
 	return (
 		<>
